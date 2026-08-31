6e1b8a5579352af7e3c0ed0ba1558fd931fa853d
acurosio03-collab <acurosio03@gmail.com>
2026-08-31 16:03:01 +0700

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,2 +1,29 @@
-window.SUPABASE_URL = "https://wwnrcdotwjxgterrhprm.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_vWecM2H8Gvurp3v4EchPRg_WCexKyqk;
+window.dbReady = false;
+window.db = null;
+
+(function () {
+  const ok =
+    window.SUPABASE_URL = "https://wwnrcdotwjxgterrhprm.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_vWecM2H8Gvurp3v4EchPRg_WCexKyqk";
+
+  if (!ok) return;
+
+  const s = document.createElement("script");
+
+  s.src = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+
+  s.onload = () => {
+    window.db = supabase.createClient(
+      window.SUPABASE_URL,
+      window.SUPABASE_ANON_KEY
+    );
+
+    window.dbReady = true;
+  };
+
+  s.onerror = () => {
+    console.error("Supabase SDK gagal dimuat.");
+  };
+
+  document.head.appendChild(s);
+})();
